fe14c136414a3bb15be2f68f868cb9075277ce3c
Uriah Harston <uharston@gmail.com>
2020-05-08 13:13:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,7 +15,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => {},
@@ -34,5 +34,5 @@ def third_challenge
    }
   }
 
-  
+
 end
